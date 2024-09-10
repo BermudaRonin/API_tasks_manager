d1e15228d625fa55ce5f8c745f41fe576a5b35e8
Naoufal <bermudaRonin@gmail.com>
2024-09-10 03:51:23 +0100

    :zap: Improve errors handling

--- src/server/services/body.service.js
+++ src/server/services/body.service.js
@@ -1,6 +1,22 @@
 import express from "express";
+import { Issue, ResponseFailure } from "../../api/v1/utils/index.js";
 
 export default class Body {
 
-    static json = express.json();
+    static config = {
+        json: {
+            verify: (req, res, buf, encoding) => {
+                try {
+                    JSON.parse(buf.toString(encoding));
+                } catch (error) {
+                    const issue = new Issue.InvalidRequest("Invalid JSON payload. Please ensure the request body is a valid JSON object.");
+                    const { status, json } = new ResponseFailure(issue)
+                    return res.status(status).json(json);
+                }
+            }
+        }
+    }
+
+
+    static json = express.json(this.config.json);
 }
